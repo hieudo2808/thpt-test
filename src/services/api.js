3a879148b02043ddf215fb1f990cc3331bfe6e39
hieudo2808 <hieudo831@gmail.com>
2025-05-14 21:11:34 +0700

    Update Axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
+  headers: new Headers({
+    "ngrok-skip-browser-warning": "69420",
+  }),
   baseURL: process.env.REACT_APP_API_URL,
 });
 
