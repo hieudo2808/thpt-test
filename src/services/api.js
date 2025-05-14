45f3e1e8fd415356ce800c4b18b8078c299cf8e7
hieudo2808 <hieudo831@gmail.com>
2025-05-14 20:59:37 +0700

    Update URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  //baseURL: "https://thpt-test-api.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
