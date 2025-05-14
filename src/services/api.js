ad94c192e9a7d556d648b7250939501ab5900237
hieudo2808 <hieudo831@gmail.com>
2025-05-14 21:21:02 +0700

    Update Axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL, // Đảm bảo REACT_APP_API_URL đã được thiết lập trong .env
   headers: {
-    "ngrok-skip-browser-warning": "69420",
+    "ngrok-skip-browser-warning": "69420", // Header bất kỳ giá trị nào cũng được ngrok chấp nhận
   },
-  baseURL: process.env.REACT_APP_API_URL,
 });
+
+api.defaults.headers.common["ngrok-skip-browser-warning"] = "69420";
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
